4a1d2a7e46e76b00e92de53d5c25bc3804d7ed59
plavskaya49 <plavskaya49@gmail.com>
2016-11-19 18:27:06 +0300

    add header html

--- www/frontend/template/header.php
+++ www/frontend/template/header.php
@@ -1,1 +1,13 @@
-<?php
+<header class="header">
+	<div align="center">
+<nav class="four">
+
+  <ul>
+	<img src="_files/dd.png" alt="" width="624" height="65">
+	    <li><a href="/">Главная</a></li>
+    <li><a href="/in.php">Войти</a></li>
+
+  </ul>
+</nav>
+</div>
+</header>
